0d83d0c67486b79e0ef94c7bb40d9125b011d602
MARCELO <marcelohenrizx@gmail.com>
2023-12-12 10:41:45 -0300

    Adcionando funcao delete mais tela de delete

--- SalesWebMvc3/Services/SellerService.cs
+++ SalesWebMvc3/Services/SellerService.cs
@@ -24,10 +24,23 @@ namespace SalesWebMvc3.Services
         }
         public void Insert(Saller obj)
         {
-            
+
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Saller FindById(int id)
+        {
+            return _context.Saller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Saller.Find(id);
+            _context.Saller.Remove(obj);
+            _context.SaveChanges();
+        }
+
 
 
 
